c030552abeee6360f49a687bc05c6018b299aefe
chiini <chiini@hotmail.de>
2019-02-19 17:24:11 +0100

    made camera detection

---
+++ Paranoia_PM/Assets/_Scripts/TriggerDetector.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TriggerDetector : MonoBehaviour {
+
+
+    public GameObject player;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == player){
+            Debug.Log("Player detected!!!");
+            //GameOver();
+        };
+    }
+
+    void GameOver(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
